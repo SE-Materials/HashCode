602dc4de599dd21960b9788494a8fa4e38f48547
Aniket Bhushan <aniket2701bhushan@gmail.com>
2020-02-21 00:09:50 +0530

    Initial commit

--- Hashcode/main.cpp
+++ Hashcode/main.cpp
@@ -7,17 +7,80 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+struct Library
+{
+    int nBooks;
+    int daysToSignup;
+    int nBooksCanShipPerDay;
+    vector<int> books;
+    
+    explicit Library(int b, int d, int days) : nBooks{b}, daysToSignup{d}, nBooksCanShipPerDay{days}
+    {
+    }
+    
+    void AddBooks(int bookId)
+    {
+        books.push_back(bookId);
+    }
+};
+
+void solve(int D, const vector<int>& bookScores, const vector<Library*>& libraries)
+{
+    int B = bookScores.size();
+    int L = libraries.size();
+    
+    // your code here
+    
+}
 
 int main(int argc, const char * argv[]) {
     
     int B{}, L{}, D{};
+    vector<int> bookScores;
+    vector<Library*> libraries;
     
     cin >> B >> L >> D;
     
+    // Books
+    bookScores.clear();
+    bookScores.resize(B);
     
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    // Libraries
+    libraries.clear();
+    libraries.resize(L);
+    
+    
+    for (int i=0; i<B; ++i)
+    {
+        cin >> bookScores[i];
+    }
+    
+    for (int iLibrary=0; iLibrary<L; ++iLibrary)
+    {
+        int nbooks, daysTosignUp, nBooksCanShip;
+        
+        cin >> nbooks >> daysTosignUp >> nBooksCanShip;
+        
+        // ith library
+        libraries[iLibrary] = new Library(nbooks, daysTosignUp, nBooksCanShip);
+        
+        string line;
+        cin >> ws;
+        getline(cin, line);
+        
+        stringstream ss(line);
+        int bookId;
+        while (ss >> bookId)
+            libraries[iLibrary]->AddBooks(bookId);
+    }
+    
+    solve(D, bookScores, libraries);
+    
     return 0;
 }
