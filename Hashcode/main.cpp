616b8a7d09a3cf009e38b9e491c2064171b8786b
Aniket Bhushan <aniket2701bhushan@gmail.com>
2020-02-20 23:54:33 +0530

    Initial Commit

---
+++ Hashcode/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  Hashcode
+//
+//  Created by Aniket Bhushan on 20/02/20.
+//  Copyright © 2020 Aniket Bhushan. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    int B{}, L{}, D{};
+    
+    cin >> B >> L >> D;
+    
+    
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
